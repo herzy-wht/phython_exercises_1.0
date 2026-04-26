d725646d2935b04b096fec32f61ade88afdc50f5
hrzy <herzwhitelead@gmail.com>
2026-04-26 23:14:22 +0800

    write the problem

---
+++ p1_evenodd_file_splitter.py
@@ -0,0 +1,5 @@
+#P-1. Write a Python program that reads a text file named numbers.
+#txt that contains 20 integers. The program will create two other text file;
+#the first text file will be named even.txt that will contains all even numbers extracted from thenumbers.txt.
+#The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt
+
