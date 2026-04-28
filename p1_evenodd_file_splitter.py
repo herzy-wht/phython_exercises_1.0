8e542cb6aaeeb95d46d233d017b076fbe6fc4f33
hrzy <herzwhitelead@gmail.com>
2026-04-28 23:45:47 +0800

    initial attempt

--- p1_evenodd_file_splitter.py
+++ p1_evenodd_file_splitter.py
@@ -1,5 +1,21 @@
-#P-1. Write a Python program that reads a text file named numbers.
-#txt that contains 20 integers. The program will create two other text file;
-#the first text file will be named even.txt that will contains all even numbers extracted from thenumbers.txt.
+#P-1. Write a Python program that reads a text file named numbers.txt
+#that contains 20 integers. The program will create two other text file; the first text file
+#will be named even.txt that will contains all even numbers extracted from the numbers.txt.
 #The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt
+
+#open and read file
+numbers_file = open('numbers.txt', 'r')
+even_file = open('even.txt', 'w')
+odd_file = open('odd.txt', 'w')
+
+for line in numbers_file:
+    number = int(line)
+    if number % 2 == 0:
+        even_file.write(line)
+    else:
+        odd_file.write(line)
+        
+even_file.close()
+odd_file.close()
+numbers_file.close()
 
