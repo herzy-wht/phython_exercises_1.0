11df41d83d0f4162d636e14b62d9faea1ce4f1d0
hrzy <herzwhitelead@gmail.com>
2026-04-29 00:06:26 +0800

    Trying to print gwa only

--- p2_highest_gwa_identifier.py
+++ p2_highest_gwa_identifier.py
@@ -1,2 +1,14 @@
 #Write a Python program that read a file containing the name of 20 students together with their GWA.
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# open and read files
+names_w_gwa = open("gwa.txt", "r")
+gwa = []
+for line in names_w_gwa:
+    for char in line:
+        if char.isdigit():
+            number = int(char)q
+            gwa.append(number)
+        else:
+            continue
+print(gwa)
