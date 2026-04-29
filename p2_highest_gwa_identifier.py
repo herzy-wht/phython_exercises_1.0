ccbffeee12cc9189fb7f8c5344c1445a250bc375
hrzy <herzwhitelead@gmail.com>
2026-04-29 10:36:01 +0800

    initial attempt

--- p2_highest_gwa_identifier.py
+++ p2_highest_gwa_identifier.py
@@ -1,28 +1,31 @@
 #Write a Python program that read a file containing the name of 20 students together with their GWA.
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-# open and read files
-names_w_gwa = open("gwa_names.txt", "r")
+class StudentGWA:
+    def find_highest(self):
+        names_w_gwa = open("gwa_names.txt", "r")
 
-highest_gwa = 0
-highest_gwa_name = ""
+        highest_gwa = 0
+        highest_gwa_name = ""
 
-for line in names_w_gwa:
-    name = ""
-    gwa = ""
+        for line in names_w_gwa:
+            name = ""
+            gwa = ""
 
-    for char in line:
-        if char.isalpha() or char == " ":
-            name += char
-        else:
-            gwa += char
+            for char in line:
+                if char.isalpha() or char == " ":
+                    name += char
+                else:
+                    gwa += char
 
-    gwa = int(gwa)
+            gwa = int(gwa)
 
-    if gwa > highest_gwa:
-        highest_gwa = gwa
-        highest_gwa_name = name
+            if gwa > highest_gwa:
+                highest_gwa = gwa
+                highest_gwa_name = name
+        names_w_gwa.close()
 
-names_w_gwa.close()
+        print(highest_gwa_name,highest_gwa)
 
-print(highest_gwa_name,highest_gwa)
+student = StudentGWA()
+student.find_highest()
--- p3_multi_line_file_writer.py
+++ p3_multi_line_file_writer.py
@@ -1,1 +1,9 @@
 #P3. Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+file_name = open("mylife.txt", "r")
+
+question = "y"
+
+while question == "y" :
+    text = str(input("Enter Line: "))
+    question = str(input("Are there more lines y/n? "))
