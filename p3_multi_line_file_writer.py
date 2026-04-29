fb03f987f0086f5418302d1078d9241ecbd2501d
hrzy <herzwhitelead@gmail.com>
2026-04-29 10:56:30 +0800

    make it OOP

--- p3_multi_line_file_writer.py
+++ p3_multi_line_file_writer.py
@@ -1,11 +1,18 @@
 #P3. Write a method in python to write multiple line of text contents into a text file mylife.txt.
 
-file_name = open("mylife.txt", "w")
+class MyLifeFile:
+    def __init__(self):
+        self.file_name = open("mylife.txt", "w")
+        self.question = "y"
 
-question = "y"
+    def write_lines(self):
+        while self.question == "y" :
+            text = str(input("Enter Line: "))
+            self.file_name.write(text)
+            self.file_name.write("\n")
+            self.question = str(input("Are there more lines y/n? "))
 
-while question == "y" :
-    text = str(input("Enter Line: "))
-    file_name.write(text)
-    file_name.write("\n")
-    question = str(input("Are there more lines y/n? "))
+        self.file_name.close()
+
+lines = MyLifeFile()
+lines.write_lines()
