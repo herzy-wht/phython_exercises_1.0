a4fdfe5abd6f261a9b0aa3b0081e83a3dbde0de3
hrzy <herzwhitelead@gmail.com>
2026-04-29 10:38:34 +0800

    make file write instead read

--- p3_multi_line_file_writer.py
+++ p3_multi_line_file_writer.py
@@ -1,6 +1,6 @@
 #P3. Write a method in python to write multiple line of text contents into a text file mylife.txt.
 
-file_name = open("mylife.txt", "r")
+file_name = open("mylife.txt", "w")
 
 question = "y"
 
